9c75532dbf8a947fbda6d16101c1313c26620cb7
KowshikChakraborty-AIUB <kowshikchakraborty6@gmail.com>
2021-03-09 22:22:26 +0600

    Dashboard Design for Teacher

---
+++ TeacherDashboard.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>Public Home</title>
+  </head>
+  <body>
+    <table border="1" cellspacing="0" width="80%" >
+      <tr>
+         <td colspan="2">
+          <table width="100%">
+              <tr>
+                <td><img height="40px" weight="40px" src="logo.png" alt=""></td>
+                <td align = "right">
+                    Logged in as
+                    <a href="ViewProfile.php"></a>|
+                    <a href="UserLogout.php">Logout</a>
+                </td>
+              </tr>
+          </table>
+          </td>
+      </tr>
+      <tr>
+        <td height="150px" weight="150px">
+            <b>Account</b>
+            <hr>
+                <ul>
+                    <li><a href="TeacherDashboard.php">Dashboard</a></li>
+                    <li><a href="ViewProfile.php">View Profile</a></li>
+                    <li><a href="EditProfile.php">View Student's Profile</a></li>
+                    <li><a href="ViewProfile.php">Student Attendance</a></li>
+                    <li><a href="ProfilePic.php">Class Schedule</a></li>
+                    <li><a href="ChangePass.php">Notice Board</a></li>
+                    <li><a href="ViewProfile.php">Upload Notes</a></li>
+                    <li><a href="ViewProfile.php">Student Marks</a></li>
+                    <li><a href="ViewProfile.php">Book History</a></li>
+                    <li><a href="UserLogout.php">Logout</a></li>
+                </ul>
+
+        </td>
+        <td><h1>Welcome</h1></td>
+      </tr>
+      <tr>
+        <td align="center" colspan="2">Copyright Ⓒ 2017</td>
+      </tr>
+    </table>
+
+  </body>
+</html>
