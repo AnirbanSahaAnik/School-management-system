936f4d8ef45c95518f9757b36e5bd49ae95fe04d
Anirban <anirbansaha002@gmail.com>
2021-03-11 22:52:53 +0600

    login

---
+++ Admin/View/adminlogin.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Home</title>
+  </head>
+  <body>
+    <table border="1" cellspacing="0" width="100%" >
+      <tr>
+        <td>
+          <table width="100%">
+            <tr>
+              <td><img height="50px" weight="50px" src="../Resources/logo.png" alt=""></td>
+              <td align = "center"><h1>School Management System</h1></td>
+              <td align = "right">
+                <a href="">About Us</a> |
+                <a href=""> Contact Us</a>
+              </td>
+            </tr>
+          </table>
+        </td>
+      </tr>
+
+
+
+
+      <tr>
+        <td align="center" >Copyright Ⓒ 2021</td>
+      </tr>
+
+    </table>
+
+  </body>
+</html>
