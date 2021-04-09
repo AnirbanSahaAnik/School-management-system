676b7dc49eae1cee9b17f1f2b8f790bdfadb4e58
BishowjitDatta <bishowjitdatta@gmail.com>
2021-04-10 02:23:04 +0600

    view profile from database

--- student/View/viewprofile.php
+++ student/View/viewprofile.php
@@ -1,6 +1,8 @@
 <?php
-$title= "Dashboard";
+$title= "View Profile";
 include('header.php');
+include_once('../model/DatabaseConnection.php');
+$viemyinfo = getUserbyid($_COOKIE['id']);
 ?>
 
 <?php include('sideBar.php'); ?> 
@@ -12,50 +14,50 @@ include('header.php');
                <tr>
                 <td>Name</td>
                 <td>:</td>
-                <td>Bishowjit Datta</td>
+                <td><?php echo $viemyinfo['name']; ?></td>
                </tr>
 
                <tr>
                 <td>Email</td> 
                 <td>:</td>
-                <td>bishowjitdatta@gmail.com</td>
+                <td><?php echo $viemyinfo['email']; ?></td>
                </tr>
 
                <tr>
                 <td>Mobile No</td> 
                 <td>:</td>
-                <td>01758621119</td>
+                <td><?php echo $viemyinfo['mobile']; ?></td>
                </tr>
               
                <tr>
                 <td>Student Id</td> 
                 <td>:</td>
-                <td>02</td>
+                <td> <?php echo $viemyinfo['id']; ?></td>
                </tr>
 
                <tr>
                 <td>Gender</td> 
                 <td>:</td>
-                <td>Male</td>
+                <td><?php echo $viemyinfo['gender']; ?></td>
                </tr>
 
 
                <tr>
                 <td>Date of Birth</td> 
                 <td>:</td>
-                <td>02-12-2010</td>
+                <td><?php echo $viemyinfo['dob']; ?></td>
                </tr>
 
                <tr>
                 <td>Present Address</td> 
                 <td>:</td>
-                <td>Banani,Dhaka</td>
+                <td><?php echo $viemyinfo['p_address']; ?></td>
                </tr>
 
                <tr>
                 <td>Class</td> 
                 <td>:</td>
-                <td>06</td>
+                <td><?php echo $viemyinfo['class']; ?></td>
                </tr>
 
                
@@ -63,14 +65,14 @@ include('header.php');
                <tr>
                 <td>Section</td> 
                 <td>:</td>
-                <td>A</td>
+                <td><?php echo $viemyinfo['section']; ?></td>
                </tr>
 
             
                <tr>
                 <td>Roll No</td> 
                 <td>:</td>
-                <td>18</td>
+                <td><?php echo $viemyinfo['roll']; ?></td>
                </tr>
 
                
