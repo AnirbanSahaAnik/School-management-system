d3ee2d943e3468f3030535cf7b8d84f8f55217c9
Anirban <anirbansaha002@gmail.com>
2021-04-10 01:11:03 +0600

    change password valid

--- Admin/Controller/chaPassCheck.php
+++ Admin/Controller/chaPassCheck.php
@@ -26,15 +26,25 @@
 			}
 			else
 			{
-				$check = changePassword($id, $newPass);
-				if($check)
-				{
-					echo "Password changed!";
-          header('location: ../Controller/logout.php');
-				}
-				else
-				{
-					echo "Error occured!";
+				if(strlen($newPass) > 7){
+					for($j=0; $j<strlen($newPass); $j++){
+						if(($newPass[$j] == '@') || ($newPass[$j] == '#') || ($newPass[$j] == '$') || ($newPass[$j] == '%') || ($newPass[$j] == '!')){
+							$check = changePassword($id, $newPass);
+							if($check)
+							{
+								echo "Password changed!";
+								header('location: ../Controller/logout.php');
+							}
+							else
+							{
+								echo "Error occured!";
+							}
+						}else {
+							echo "Password must contain a special character";
+						}
+					}
+				}else {
+					echo "Password length should be greater than 7";
 				}
 			}
 		}
