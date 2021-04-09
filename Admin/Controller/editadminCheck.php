d03a8acb79757b1c1ff67c0005dd4e32d1e8c1d1
Anirban <anirbansaha002@gmail.com>
2021-04-10 00:40:45 +0600

    update user validation

--- Admin/Controller/editadminCheck.php
+++ Admin/Controller/editadminCheck.php
@@ -8,16 +8,29 @@
     $email = $_POST['email'];
 
 
-		$userinfo = array('id' => $id,'name' => $name, 'email' => $email);
-		$check = updateMyInfo($id, $userinfo);
-		if($check)
-		{
-			echo "info updated!";
-			header('location: ../view/EditProfile.php');
+		for($i=0; $i<strlen($name); $i++){
+			if((ord($name[$i]) >= 97 && ord($name[$i]) <= 122) || (ord($name[$i]) >= 65 && ord($name[$i]) <= 90) || ord($name[$i]) == ' '){
+				if(strlen($name) > 2){
+					$userinfo = array('id' => $id,'name' => $name, 'email' => $email);
+					$check = updateMyInfo($id, $userinfo);
+					if($check)
+					{
+						echo "info updated!";
+						header('location: ../view/EditProfile.php');
+					}
+					else
+					{
+						echo "Can't update the Information!";
+					}
+
+				}else {
+					echo "Name length should be greater than 2";
+				}
+			}else {
+				echo "Name contain only alphanumeric characters";
+			}
 		}
-		else
-		{
-			echo "Can't update the Information!";
-		}
+
+
 	}
 ?>
--- Admin/Controller/updateCheckStudent.php
+++ Admin/Controller/updateCheckStudent.php
@@ -14,16 +14,31 @@
     $section = $_POST['section'];
     $roll = $_POST['roll'];
 
-		$userinfo = array('id' => $id,'name' => $name, 'email' => $email, 'mobile' => $mobile,'gender' => $gender,'dob' => $dob,'p_address' => $p_address,'class' => $class,'section' => $section,'roll' => $roll);
-		$check = updateMyInfo($id, $userinfo);
-		if($check)
-		{
-			echo "info updated!";
-			header('location: ../view/viewStudent.php');
+		if(strlen($mobile) == 11){
+			for($i=0; $i<strlen($name); $i++){
+				if((ord($name[$i]) >= 97 && ord($name[$i]) <= 122) || (ord($name[$i]) >= 65 && ord($name[$i]) <= 90) || ord($name[$i]) == ' '){
+					if(strlen($name) > 2){
+						$userinfo = array('id' => $id,'name' => $name, 'email' => $email, 'mobile' => $mobile,'gender' => $gender,'dob' => $dob,'p_address' => $p_address,'class' => $class,'section' => $section,'roll' => $roll);
+						$check = updateMyInfo($id, $userinfo);
+						if($check)
+						{
+							echo "info updated!";
+							header('location: ../view/viewStudent.php');
+						}
+						else
+						{
+							echo "Can't update the Information!";
+						}
+					}else {
+						echo "Name length should be greater than 2";
+					}
+				}else {
+					echo "Name contain only alphanumeric characters";
+				}
+			}
+		}else {
+			echo "Mobile must contain 11 digits and integer number only";
 		}
-		else
-		{
-			echo "Can't update the Information!";
-		}
+
 	}
 ?>
