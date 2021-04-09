e3214592bb87c035bd28abc77cc23d5e9196c9e0
Anirban <anirbansaha002@gmail.com>
2021-04-09 17:17:23 +0600

    Fix: validation teacher

--- Admin/Controller/regCheckTeacher.php
+++ Admin/Controller/regCheckTeacher.php
@@ -21,112 +21,57 @@
 		if($id == "" || $name == "" || $email == "" || $password == "" || $repass == ""|| $mobile == ""|| $gender == ""|| $dob == ""){
 			echo "null submission...";
 		}else{
-
 			if($password == $repass){
-        function FormValidation()
-                        {
-                          if(isset($_POST['name'])){
+        if(strlen($id) == 4){
+          if(strlen($mobile) == 11){
+            if(strlen($password) > 7){
+              for($j=0; $j<strlen($password); $j++){
+                if(($password[$j] == '@') || ($password[$j] == '#') || ($password[$j] == '$') || ($password[$j] == '%')){
+                  for($i=0; $i<strlen($name); $i++){
+                    if((ord($name[$i]) >= 97 && ord($name[$i]) <= 122) || (ord($name[$i]) >= 65 && ord($name[$i]) <= 90)
+                    || (ord($name[$i]) >= 48 && ord($name[$i]) <= 57) || ($name[$i] == '.') || ($name[$i] == '-') || ($name[$i] == '_')){
+                      if(strlen($name) > 2){
+                        $userinfo = [
+                            'name'=>$name,
+                            'mobile'=>$mobile,
+                            'email'=> $email,
+                            'id'=>$id,
+                            'password'=>$password,
+                            'gender'=>$gender,
+                            'dob'=>$dob
+                        ];
 
+                        $result = insert($userinfo);
+                        if($result){
+                          header('location: ../view/addTeacher.php');
+                        }else{
+                          echo "not insert";
+                        }
 
-                            $name = $_POST['name'];
-                            $email = $_POST['email'];
-                            $mobile = $_POST['mobile'];
-                            $id = $_POST['id'];
-                            $password = $_POST['password'];
-                            $gender = $_POST['gender'];
-                            $dob = $_POST['dob'];
-                              $Validation = false;
+                      }else {
+                        echo "Name length should be greater than 2";
+                      }
+                    }else {
+                      echo "Name contain only alphanumeric characters";
+                    }
+                  }
 
-                                if(strlen($id) == 4)
-                                {
+                }else {
+                  echo "Password must contain a special character";
+                }
+              }
+            }else {
+              echo "Password length should be greater than 7";
+            }
 
-                                    if(strlen($mobile) == 11)
-                                {
+          }else {
+            echo "Mobile must contain 11 digits and integer number only";
+          }
 
-                                    if(strlen($password) > 7)
-                                {
-
-                                    for($j=0; $j<strlen($password); $j++)
-                                    {
-
-
-                                        if(($password[$j] == '@') || ($password[$j] == '#') || ($password[$j] == '$') || ($password[$j] == '%'))
-                                        {
-
-
-
-
-                                               for($i=0; $i<strlen($name); $i++)
-                                               {
-
-                                                   if((ord($name[$i]) >= 97 && ord($name[$i]) <= 122) || (ord($name[$i]) >= 65 && ord($name[$i]) <= 90) || (ord($name[$i]) >= 48 && ord($name[$i]) <= 57) || ($name[$i] == '.') || ($name[$i] == '-') || ($name[$i] == '_'))
-                                                   {
-                                                     if(strlen($name) > 2)
-                                                     {
-
-                                                       $Validation = true;
-
-
-                                                        $userinfo = [
-
-                                                              'name'=>$name,
-                                                              'mobile'=>$mobile,
-                                                              'email'=> $email,
-                                                              'id'=>$id,
-                                                              'password'=>$password,
-                                                              'gender'=>$gender,
-                                                              'dob'=>$dob
-
-                                                           ];
-
-                                                          $result = insert($userinfo);
-                                                          if($result){
-                                                              header('location: ../view/addTeacher.php');
-                                                          }else{
-                                                              echo "not insert";
-                                                          }
-
-                                                    }
-
-                                               }
-                                               else{
-                                                 echo "Username not valid (may contain alphanumeric characters, period, dash or underscore only and length should be greater than 2) ";
-                                                  }
-
-                                             }
-
-                                        }else{
-                                            echo "Password not valid (must contain a special character)";
-                                        }
-
-                                    }
-
-                                }else{
-                                    echo "Password not valid (length should be greater than 7) ";
-                                }
-
-
-                                }else{
-                                    echo "Mobile number not valid (must contain 11 digits and integer number only) ";
-                                }
-
-
-                                }else{
-                                    echo "ID not valid (must contain 03 digits and integer number only) ";
-                                }
-
-
-                            }
-                          }
-                          if(FormValidation())
-                            {
-                                true;
-                            }
-
-
-
-			}
-      else{
+        }else {
+          echo "ID must contain 04 digits and integer number only";
+        }
+			}else{
 				echo "password & confirm password mismatch..";
 			}
 		}
