9ed0dc621565dc0c7fe995abdd410be35247ddf8
Anirban <anirbansaha002@gmail.com>
2021-04-10 14:06:01 +0600

    Libary Book list from database

--- Admin/View/libraryBook.php
+++ Admin/View/libraryBook.php
@@ -1,77 +1,39 @@
 <?php
 	$title= "Library Book";
 	include('header.php');
+	include_once('../model/bookModel.php');
+	$UsersList = allUserList();
 ?>
                 <ul>
                   <li><a href="libraryBook.php">Library Book list</a></li>
-
-
                 </ul>
               </td>
-
               <td>
                 <center><h2>Library Book list</h2></center>
 
-                <table width="100%" border="1">
-                  <tr align="center" >
-                    <td>SL. No.</td>
-                    <td>Name</td>
-                    <td>Author</td>
-                  </tr>
+								<?php
+								echo "<table border = 1 width='100%' cellspacing = 0  >
+								<tr align = 'center'>
+										<td>ISBN</td>
+										<td>Title</td>
+										<td>Author</td>
+										<td>Edition</td>
+								</tr>";
+								for($i = 0; $i<count($UsersList); $i++){
+										echo "<tr align = 'center'>
+										<td>{$UsersList[$i]['isbn']}</td>
+										<td>{$UsersList[$i]['title']}</td>
+										<td>{$UsersList[$i]['author']}</td>
+										<td>{$UsersList[$i]['edition']}</td>
+								</tr>";
+								}
+								echo "</table>";
+								?>
 
-                  <tr align="center">
-                    <td>1</td>
-                    <td>Get Real and Get In: How to Get Into the College of Your Dreams by Being Your Authentic Self</td>
-                    <td>Dr. Aviva Legatt</td>
-                  </tr>
-
-                  <tr align="center">
-                    <td>2</td>
-                    <td>A Child Called "It" : One Child's Courage To Survive</td>
-                    <td>elzer, David J</td>
-                  </tr>
-
-                  <tr align="center">
-                    <td>3</td>
-                    <td>I Know Why The Caged Bird Sings</td>
-                    <td>Angelou, Maya</td>
-                  </tr>
-
-                  <tr align="center">
-                    <td>4</td>
-                    <td>The Alchemist</td>
-                    <td>Paulo Coelho</td>
-                  </tr>
-
-                  <tr align="center">
-                    <td>5</td>
-                    <td>Aristotle and Dante Discover the Secrets of the Universe</td>
-                    <td>Benjamin Alire Sáenz</td>
-                  </tr>
-
-                  <tr align="center">
-                    <td>6</td>
-                    <td>A Prayer for Owen Meany</td>
-                    <td>John Irving</td>
-                  </tr>
-
-                  <tr align="center">
-                    <td>7</td>
-                    <td>A Tree Grows in Brooklyn</td>
-                    <td>Betty Smith</td>
-                  </tr>
-
-                  <tr align="center">
-                    <td colspan="4"><a href="#">See More</a></td>
-                  </tr>
-                </table>
-              </td>
+							</td>
             </tr>
           </table>
         </td>
       </tr>
 
-
-
-
-      <?php include('footer.php'); ?>
+<?php include('footer.php'); ?>
