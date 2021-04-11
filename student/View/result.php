df3dbdcee9b2af977b76b46c1b4b46c8e5807d8a
BishowjitDatta <bishowjitdatta@gmail.com>
2021-04-12 00:31:41 +0600

    view result

--- student/View/result.php
+++ student/View/result.php
@@ -1,154 +1,32 @@
 <?php
 $title= "Dashboard";
 include('header.php');
+include_once('../model/DatabaseConnection.php');
+$viewmyresult = getUserbyid($_COOKIE['id']);
 ?>
 
 <?php include('sideBar.php'); ?> 
         <td>
-            <table border="1">
-                <thead>
-                <tr>
-                  <th>Subject</th>
-                  <th>Assignment(20)</th>
-                  <th>Class Test(20)</th>
-                  <th>Mid Term Marks(60)</th>
-                  <th>Total</th>
-                <tr>  
-    
-                </thead>
-              
-              <tbody>
-                <tr>
-                  <td>English</td>
-                  <td align="center">15</td>
-                  <td align="center">20</td>
-                  <td align="center">50</td>
-                  <td align="center">85</td>
-                  
-                </tr>
-                <tr>
-                    <td>Math</td>
-                    <td align="center">15</td>
-                    <td align="center">20</td>
-                    <td align="center">50</td>
-                    <td align="center">85</td>
-                </tr>
-                <tr>
-                    <td>Bangla</td>
-                    <td align="center">20</td>
-                    <td align="center">20</td>
-                    <td align="center">55</td>
-                    <td align="center">95</td>
-                </tr>
-                <tr>
-                    <td>Bangladesh and global studies</td>
-                    <td align="center">20</td>
-                    <td align="center">20</td>
-                    <td align="center">55</td>
-                    <td align="center">95</td>
-                </tr>
-                <tr>
-                    <td>Physical education and health</td>
-                    <td align="center">20</td>
-                    <td align="center">20</td>
-                    <td align="center">55</td>
-                    <td align="center">95</td>
-                </tr>
-                <tr>
-                    <td>Religion</td>
-                    <td align="center">20</td>
-                    <td align="center">20</td>
-                    <td align="center">55</td>
-                    <td align="center">95</td>
-                </tr>
-                
-              </tbody>
-              <footer>
-              <tfoot>
-                <tr>
-                  <td colspan=1>Total Marks: 600 </td>
-                  <td colspan=2>Marks Obtained: 550 </td>
-                  <td colspan=2>Division: 1st Division </td>
-                </tr>
-              </tfoot>
-            </footer>
-            </table>
-    
-    
-    
-    
-    
-    
-            <table border="1">
-                <thead>
-                  <tr>
-                    <th>Subject</th>
-                    <th>Assignment(20)</th>
-                    <th>Class Test(20)</th>
-                    <th>Final Term Marks(60)</th>
-                    <th>Total</th>
-                  <tr>  
-                </thead>
-                <tbody>
-                  <tr>
-                    <td>English</td>
-                    <td align="center">00</td>
-                    <td align="center">00</td>
-                    <td align="center">00</td>
-                    <td align="center">00</td>
-                    
-                  </tr>
-                  <tr>
-                      <td>Math</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                  </tr>
-                  <tr>
-                      <td>Bangla</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                  </tr>
-                  <tr>
-                      <td>Bangladesh and global studies</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                  </tr>
-                  <tr>
-                      <td>Physical education and health</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                  </tr>
-                  <tr>
-                      <td>Religion</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                      <td align="center">00</td>
-                  </tr>
-                  
-                </tbody>
-                <footer>
-                <tfoot>
-                  <tr>
-                    <td colspan=1>Total Marks: </td>
-                    <td colspan=2>Marks Obtained: </td>
-                    <td colspan=2>Division: </td>
-                  </tr>
-                  <td align = "left">
-                    <a href="dashboard.html">Back</a>
-                    <a href="">Download</a>
-                  </td>
-                </tfoot>
-                </footer>
-              </table>
+            <fieldset>
+                <legend>MY Result</legend>
+            <form class="" action="" method="post"> 
+               <table>
+               <tr>
+                <td>Name</td>
+                <td>:</td>
+                <td><?php echo $viewmyresult['name']; ?></td>
+               </tr>
+
+               <tr>
+                <td>Marks</td> 
+                <td>:</td>
+                <td><?php echo $viewmyresult['marks']; ?></td>
+               </tr>
+
+               </table>
+               
+            </form>
+            </fieldset>
         </td>
       </tr>
 
