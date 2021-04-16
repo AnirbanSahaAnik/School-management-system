f72a62bde8a6780396b10a7f07ec95e8f274c46e
Anirban <anirbansaha002@gmail.com>
2021-04-16 22:11:00 +0600

    Book Info search by AJAX

--- Admin/View/libraryBook.php
+++ Admin/View/libraryBook.php
@@ -1,5 +1,6 @@
 <?php
 	$title= "Library Book";
+	$js = "../script/BookSearch.js";
 	include('header.php');
 	include_once('../model/bookModel.php');
 	$UsersList = allUserList();
@@ -9,26 +10,34 @@
                 </ul>
               </td>
               <td>
-                <center><h2>Library Book list</h2></center>
+                <center><h2  >Library Book list</h2>
+								<input type="text" name="name" id="name" onkeyup="ajax()" />
+								<input type="button" name="" value="Search">
+								<!-- <div id="result">
 
-								<?php
-								echo "<table border = 1 width='100%' cellspacing = 0  >
-								<tr align = 'center'>
-										<td>ISBN</td>
-										<td>Title</td>
-										<td>Author</td>
-										<td>Edition</td>
-								</tr>";
-								for($i = 0; $i<count($UsersList); $i++){
-										echo "<tr align = 'center'>
-										<td>{$UsersList[$i]['isbn']}</td>
-										<td>{$UsersList[$i]['title']}</td>
-										<td>{$UsersList[$i]['author']}</td>
-										<td>{$UsersList[$i]['edition']}</td>
-								</tr>";
-								}
-								echo "</table>";
-								?>
+								</div> -->
+								</center>
+								<br>
+								<div id="myh1" class="">
+									<?php
+									echo "<table  border = 1 width='100%' cellspacing = 0  >
+									<tr align = 'center'>
+											<td>ISBN</td>
+											<td>Title</td>
+											<td>Author</td>
+											<td>Edition</td>
+									</tr>";
+									for($i = 0; $i<count($UsersList); $i++){
+											echo "<tr align = 'center'>
+											<td>{$UsersList[$i]['isbn']}</td>
+											<td>{$UsersList[$i]['title']}</td>
+											<td>{$UsersList[$i]['author']}</td>
+											<td>{$UsersList[$i]['edition']}</td>
+									</tr>";
+									}
+									echo "</table>";
+									?>
+								</div>
 
 							</td>
             </tr>
