bc3371c5085045506cba44e2b8ad55378ab75c8c
kaderabdul22 <mostafakamal9300@gmail.com>
2021-04-24 23:01:24 +0600

    design the login page

--- Librarian/View/librarianLogin.php
+++ Librarian/View/librarianLogin.php
@@ -2,6 +2,7 @@
 <html>
 <head>
 	<title>Login Page</title>
+  
 	<style>
         .error{
             border: 2px solid red;
@@ -9,7 +10,40 @@
         .success{
             border: 2px solid green;
         }
-    </style>
+        
+#loginform table {
+  background-color: #fff;
+  padding: 50px 10px;
+}
+
+#loginform table .librarianid td, #loginform table .librarianpass td{
+  font-size: 20px;
+
+}
+#loginform table input{
+  font-size: 20px;
+
+}
+#loginform #loginbttn{
+  padding: 12px 30px;
+  background-color: grey;
+
+}
+#loginform #loginbttn:hover{
+  background-color: lightblue;
+  border-radius: 8px;
+
+}
+#loginform a{
+  font-size:20px;
+  margin-left: 140px;
+}
+#loginform a:hover{
+  color: green;
+}
+
+</style>
+    
 </head>
 <body> 
 <script>
@@ -49,7 +83,7 @@
 
 }
 </script>
-    <table border="1" cellspacing="0" width="100%" >
+<table border="1" cellspacing="0" width="100%" >
       <tr>
         <td>
           <table width="100%">
@@ -68,29 +102,31 @@
           <table align="center">
             <tr>
               <td>
+                <div id="loginform">
                 <form class="" action="../Controller/librarianLogCheck.php" method="POST" onsubmit="return Librarianlogin()">
                   <fieldset>
                     <legend>LOGIN</legend>
                     <table>
-                      <tr>
+                      <tr class="librarianid">
                         <td>Librarian ID</td>
                         <td>: <input type="text" name="librarianid" id="librarianid" value=""  onkeypress="validation()"><div id="msg1"></div></td>
                       </tr>
-                      <tr>
+                      <tr class="librarianpass">
                         <td>Password</td>
                         <td>: <input type="password" name="password" id="password" value=""  onkeypress="validation()"><div id="msg2"></div></td>
                       </tr>
                     </table>
                     <!-- <input type="checkbox" name="remember" value=""> Remember Me <br> -->
                     <br>
-                    <input type="submit" name="login" value="LOGIN">
+                    <input type="submit" name="login" value="LOGIN" id="loginbttn">
                     <a href="librarianReg.php">Registration</a>
                     <div id="login"></div>
                   </fieldset>
                 </form>
+                </div>
             </td>
           </tr>
           </table>
         </td>
       </tr>
-<?php include('footer.php'); ?>   
+<?php include('footer.php'); ?> 
